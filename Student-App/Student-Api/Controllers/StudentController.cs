406fc40487be82afa355654f3b277b2a6cc5c628
Jahid Uddin <jahiduddin922@gmail.com>
2022-04-21 23:32:23 +0600

    Add new Controller

---
+++ Student-App/Student-Api/Controllers/StudentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+    }
+}
