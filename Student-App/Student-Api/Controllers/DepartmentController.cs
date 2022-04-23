31f0fdb409fcc968ed3a256199eb8c41ff6bba8d
Jahid Uddin <jahiduddin922@gmail.com>
2022-04-23 14:46:07 +0600

    Add Api Versioning

--- Student-App/Student-Api/Controllers/DepartmentController.cs
+++ Student-App/Student-Api/Controllers/DepartmentController.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Student_Api.Controllers
 {
-    [Route("api/[controller]")]
+   
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
     [ApiController]
     public class DepartmentController : ControllerBase
     {
@@ -11,6 +13,26 @@ namespace Student_Api.Controllers
         public IActionResult GetAll()
         {
             return Ok("get all data");
+        }
+        [HttpGet("{code}")]
+        public IActionResult GetA(string code)
+        {
+            return Ok("get a "+ code +"data");
+        }
+        [HttpPost]
+        public IActionResult Insert()
+        {
+            return Ok("Insert new department");
+        }
+        [HttpPut("{code}")]
+        public IActionResult Update (string code)
+        {
+            return Ok("update" +code);
+        }
+        [HttpDelete("{code}")]
+        public IActionResult Delete(string code)
+        {
+            return Ok("delete" +code);
         }
     }
 }
