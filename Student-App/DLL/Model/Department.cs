7e5e412c52c8a51a88e383a7b9f882a8987a46e8
Jahid Uddin <jahiduddin922@gmail.com>
2022-04-25 15:16:24 +0600

    Add New Model

---
+++ Student-App/DLL/Model/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DLL.Model
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        [Column(TypeName = "nvarchar(15)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string Code { get; set; }
+
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public DateTime CreatedDate { get; internal set; }
+    }
+}
---
+++ Student-App/DLL/Model/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DLL.Model
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Column(TypeName = "nvarchar(15)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(20)")]
+        public string Email { get; set; }
+
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public DateTime CreatedDate { get; internal set; }
+    }
+}
